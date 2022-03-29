52ba71f2f139b8cc83dafbba64e4b5f245d366f6
Daniel E. Sejersen <Daniel.sejersen@hotmail.com>
2022-03-29 19:28:56 +0200

    trinvis forfinelse i funktioner

--- client/js/game.js
+++ client/js/game.js
@@ -7,15 +7,16 @@ const formMenu = document.getElementById("form-menu");
 const spawnBtn = document.getElementById("spawn");
 
 
-//set canvas size to window size.
-ctx.canvas.width = window.innerWidth;
-ctx.canvas.height = window.innerHeight;
 
-ctx.font = "30px Arial";
+
+
 
 
 let myId;
 let localWorld;
+
+getServerData();
+sendClientData();
 
 
 
@@ -30,11 +31,11 @@ function drawElements(data) {
 
 }
 
-
 function drawPickUpToolTip(data) {
     for (const key in data.players) {
         //only draws for the current clients player
         if (data.players[key].id === myId && data.players[key].isColliding === true && isEmpty(data.players[key].connectedEntity) === true) {
+            ctx.font = "30px Arial";
             ctx.textAlign = "center";
             ctx.fillStyle = "black";
             ctx.fillText("Press E to interact", data.players[key].x, data.players[key].y - 35)
@@ -42,10 +43,9 @@ function drawPickUpToolTip(data) {
     }
 }
 
-
 function drawPlayers(data) {
     for (const key in data.players) {
-        //draw green circle
+        //draw player circle
         ctx.beginPath();
         ctx.arc(data.players[key].x, data.players[key].y, data.players[key].h / 2, 0, 2 * Math.PI);
         ctx.fillStyle = data.players[key].color;
@@ -54,11 +54,13 @@ function drawPlayers(data) {
         ctx.strokeStyle = 'black';
         ctx.stroke();
 
+        //draw name
+        ctx.font = "30px Arial";
         ctx.textAlign = "center";
         ctx.fillStyle = "black";
         ctx.fillText(data.players[key].name, data.players[key].x, data.players[key].y + 65)
 
-        //draw entities on player
+        //draw picked up entity
         if (isEmpty(data.players[key].connectedEntity) === false) {
             ctx.beginPath();
             ctx.rect(data.players[key].connectedEntity.x, data.players[key].connectedEntity.y, data.players[key].connectedEntity.h, data.players[key].connectedEntity.w);
@@ -68,121 +70,127 @@ function drawPlayers(data) {
     }
 }
 
+function renderCanvas() {
+    //set canvas size to window size.
+    ctx.canvas.width = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    drawElements(localWorld);
+    drawPlayers(localWorld);
+    drawPickUpToolTip(localWorld);
+}
+
+function sendClientData() {
+
+    spawnBtn.addEventListener("click", (e) => {
+        socket.emit("spawnElement");
+    });
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let name = form.elements['name'].value;
+        let color = form.elements["color"].value;
+        formMenu.style = "display: none;";
+        socket.emit("submit-form", {
+            name: name,
+            color: color,
+        })
+    });
+
+    document.onkeydown = (event) => {
+        if (event.key === 'd' || event.key === 'D') {
+            socket.emit('keyPress', {
+                inputId: 'right',
+                state: true
+            })
+        } else if (event.key === 's' || event.key === 'S') {
+            socket.emit('keyPress', {
+                inputId: 'down',
+                state: true
+            })
+        } else if (event.key === 'a' || event.key === 'A') {
+            socket.emit('keyPress', {
+                inputId: 'left',
+                state: true
+            })
+        } else if (event.key === 'w' || event.key === 'W') {
+            socket.emit('keyPress', {
+                inputId: 'up',
+                state: true
+            })
+        } else if (event.key === 'e' || event.key === 'E') {
+            socket.emit('keyPress', {
+                inputId: 'pickUpKeyPressed',
+                state: true
+            })
+        }
+    }
+
+    document.onkeyup = (event) => {
+        if (event.key === 'd' || event.key === 'D') {
+            socket.emit('keyPress', {
+                inputId: 'right',
+                state: false
+            })
+        } else if (event.key === 's' || event.key === 'S') {
+            socket.emit('keyPress', {
+                inputId: 'down',
+                state: false
+            })
+        } else if (event.key === 'a' || event.key === 'A') {
+            socket.emit('keyPress', {
+                inputId: 'left',
+                state: false
+            })
+        } else if (event.key === 'w' || event.key === 'W') {
+            socket.emit('keyPress', {
+                inputId: 'up',
+                state: false
+            })
+        } else if (event.key === 'e' || event.key === 'E') {
+            socket.emit('keyPress', {
+                inputId: 'pickUpKeyPressed',
+                state: false
+            })
+        }
+    }
+
+}
+
+function getServerData() {
+
+    socket.on("sendId", (data) => {
+        myId = data;
+        console.log(`my socket ID is: ${myId}`);
+    });
+
+    socket.on("newPosistion", (data) => {
+
+        //update local world storage
+        localWorld = data;
+        //render new update
+        renderCanvas();
+    });
+}
 
 
+//----helper functions---
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0;
+}
 
 canvas.onmousemove = function (e) {
     // Get the current mouse position
     var r = canvas.getBoundingClientRect(),
         x = e.clientX - r.left, y = e.clientY - r.top;
     hover = false;
-    if(!isEmpty(localWorld.entities)) {
+    if (!isEmpty(localWorld.entities)) {
         for (const key in localWorld.entities) {
             if (x >= localWorld.entities[key].x && x <= localWorld.entities[key].x + localWorld.entities[key].w && y >= localWorld.entities[key].y && y <= localWorld.entities[key].y + localWorld.entities[key].h) {
                 hover = true
                 console.log(`I FOUND ENTITY: ${localWorld.entities[key].id}`);
+                break;
             }
         }
     }
-    
-
-}
-
-socket.on("sendId", (data) => {
-    myId = data;
-    console.log(`my socket ID is: ${myId}`);
-});
-
-socket.on("newPosistion", (data) => {
-    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-    //update local world storage
-    localWorld = data;
-
-    drawElements(data);
-    drawPlayers(data);
-
-    drawPickUpToolTip(data);
-
-
-});
-
-
-spawnBtn.addEventListener("click", (e) => {
-    socket.emit("spawnElement");
-});
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let name = form.elements['name'].value;
-    let color = form.elements["color"].value;
-    formMenu.style = "display: none;";
-    socket.emit("submit-form", {
-        name: name,
-        color: color,
-    })
-});
-
-document.onkeydown = (event) => {
-    if (event.key === 'd' || event.key === 'D') {
-        socket.emit('keyPress', {
-            inputId: 'right',
-            state: true
-        })
-    } else if (event.key === 's' || event.key === 'S') {
-        socket.emit('keyPress', {
-            inputId: 'down',
-            state: true
-        })
-    } else if (event.key === 'a' || event.key === 'A') {
-        socket.emit('keyPress', {
-            inputId: 'left',
-            state: true
-        })
-    } else if (event.key === 'w' || event.key === 'W') {
-        socket.emit('keyPress', {
-            inputId: 'up',
-            state: true
-        })
-    } else if (event.key === 'e' || event.key === 'E') {
-        socket.emit('keyPress', {
-            inputId: 'pickUpKeyPressed',
-            state: true
-        })
-    }
-}
-
-
-
-document.onkeyup = (event) => {
-    if (event.key === 'd' || event.key === 'D') {
-        socket.emit('keyPress', {
-            inputId: 'right',
-            state: false
-        })
-    } else if (event.key === 's' || event.key === 'S') {
-        socket.emit('keyPress', {
-            inputId: 'down',
-            state: false
-        })
-    } else if (event.key === 'a' || event.key === 'A') {
-        socket.emit('keyPress', {
-            inputId: 'left',
-            state: false
-        })
-    } else if (event.key === 'w' || event.key === 'W') {
-        socket.emit('keyPress', {
-            inputId: 'up',
-            state: false
-        })
-    } else if (event.key === 'e' || event.key === 'E') {
-        socket.emit('keyPress', {
-            inputId: 'pickUpKeyPressed',
-            state: false
-        })
-    }
-}
-
-function isEmpty(obj) {
-    return Object.keys(obj).length === 0;
 }
