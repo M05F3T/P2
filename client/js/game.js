b5c7b3c95b73b4ee4865181948e7cfc57fb7fe00
Daniel E. Sejersen <Daniel.sejersen@hotmail.com>
2022-05-16 11:42:06 +0200

    prevent user from zooming

--- client/js/game.js
+++ client/js/game.js
@@ -57,6 +57,22 @@ function sendTokenInformationForVerify()
     socket.emit("denial", window.location.href);
 }
 
+document.addEventListener("keydown",  function(event) {
+    if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+            event.preventDefault();
+         }
+        // 107 Num Key  +
+        // 109 Num Key  -
+        // 173 Min Key  hyphen/underscore key
+        // 61 Plus key  +/= key
+    });
+    
+    window.addEventListener('mousewheel DOMMouseScroll', function (event) {
+           if (event.ctrlKey == true) {
+           event.preventDefault();
+           }
+    });
+
 function sendClientData() {
 
 
